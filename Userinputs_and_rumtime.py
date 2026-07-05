fe399bdd737b8237a2d05ac47c7a7ba1d76cf100
Prabhas133 <152586028+Prabhas133@users.noreply.github.com>
2026-07-05 10:32:19 +0530

    Add files via upload

---
+++ Userinputs_and_rumtime.py
@@ -0,0 +1,125 @@
+#Without using a user input adding a values
+#print(2+3)
+'''a= 5
+b=5
+print(5+5)'''
+'''a = 4
+b = 5
+print(4+5)'''
+
+#run time input()
+#For integer
+'''a = int(input("Enter the value a : "))
+b = int(input("Enter the value b : "))
+print(a+b)'''
+
+#For float
+'''a = float(input("Enter the value a : "))
+b = float(input("Enter the value b : "))
+print(a+b)'''
+
+#for str
+#There are 2 method to declare to str
+'''a = str(input("Enter the value a : "))
+b = str(input("Enter the value b : "))
+print(a+b)'''
+#OR
+'''a = input("Enter the value a : ")
+b = input("Enter the value b : ")
+print(a+b)'''
+
+'''fname = input("Enter the value a : ")
+lname = input("Enter the value b : ")
+print((fname + " " +lname ).title())'''
+
+#for complex
+'''a = complex(input("Enter the value a : "))
+b = complex(input("Enter the value b : "))
+print(a+b)'''#Real and imaginer output
+
+#for Boolean
+'''a = bool(input("Enter the value a : "))
+b = bool(input("Enter the value b : "))
+print(a+b)'''#In consider the output, will be how many variables assigned as bool that count and output : 2 - because declare two booleans.if 3 it print output : 3.
+
+#User choice
+'''a = int(input("Enter the value a : "))
+b = int(input("Enter the value b : "))
+option = int(input("choose the option 1.add 2.sub 3.mul : "))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a = int(input("Enter the value a : "))
+b = int(input("Enter the value b : "))
+option = int(input(''''''choose the option
+                   1.add
+                   2.sub
+                   3.mul :''''''))
+print(a+b)
+print(a-b)
+print(a*b)'''#if multiple line means use ''' codes
+
+
+'''a = int(input("Enter the value a : "))
+b = int(input("Enter the value b : "))
+option = input("choose the option add sub mul : ")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a = input()
+b = input()
+print(a+b)'''
+
+#swapping Task and 4 methods
+'''a = int(input())
+b = int(input())'''
+'''temp = a
+a = b
+b = temp
+print(a,b)'''#method1 temp method
+
+#method2
+'''a = a+b
+b=a-b
+a=a-b
+print(a,b)''' #method opeartor method
+#OR
+'''a = a ^ b
+b = a^b
+a = a^b'''
+
+#method3 using number format 
+'''a = a+b
+b=a-b
+a=a-b
+print("a = %d , b = %d" %(a,b))'''
+
+'''a,b = b,a
+print(a,b)'''#methods4 normal swapping
+
+#uing float
+'''c = float(input())
+d = float(input())
+c = c+d
+d=c-d
+c=c-d
+print("c = %.1f , d = %.1f" % (c,d))'''
+
+#using string
+'''e = str(input())
+f = str(input())
+e,f = f,e or temp method we can use 
+print("a = %s , b = %s" %(e,f))'''
+
+
+
+
+
+
+
+
+
+
+
